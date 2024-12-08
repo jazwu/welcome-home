7be697ec612bd771a59dccfa3cef39c91f9361df
jazwu <jaz.wu@outlook.com>
2024-12-08 18:53:12 -0500

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
+import FindItem from "./pages/FindItem";
+import FindOrder from "./pages/FindOrder";
+import DisplayItems from "./pages/DisplayItems";
+import AcceptDonation from "./pages/AcceptDonation";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     <p>Hello World</p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/find-item" element={<FindItem />} />
+        <Route path="/find-order" element={<FindOrder />} />
+        <Route path="/display-items" element={<DisplayItems />} />
+        <Route path="/accept-donation" element={<AcceptDonation />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/FindOrder.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FindOrder() {
+  return (
+    <div>FindOrder</div>
+  )
+}
