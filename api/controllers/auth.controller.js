7596bdb7418d64d4e6101c1c8ecc9c299a601935
jazwu <jaz.wu@outlook.com>
2024-12-09 01:27:40 -0500

    feat(api): create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import db from "../db.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const signup = (req, res, next) => {
   const { username, password, fname, lname, email, role } = req.body;
@@ -28,6 +32,61 @@ export const signup = (req, res, next) => {
         }
       );
       res.status(201).json({ message: "User added" });
+    }
+  );
+};
+
+export const signin = (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    next(errorHandler({ statusCode: 400, message: "All fields are required" }));
+  }
+
+  db.query(
+    "SELECT * FROM Person WHERE userName = ?",
+    [username],
+    (error, results) => {
+      if (error) {
+        next(errorHandler({ statusCode: 500, message: error.message }));
+      }
+
+      if (results.length === 0) {
+        next(errorHandler({ statusCode: 404, message: "User not found" }));
+      }
+
+      const user = results[0];
+
+      if (!bcryptjs.compareSync(password, user.password)) {
+        next(errorHandler({ statusCode: 400, message: "Invalid password" }));
+      }
+
+      db.query(
+        "SELECT * FROM Act WHERE userName = ?",
+        [username],
+        (error, results) => {
+          if (error) {
+            next(errorHandler({ statusCode: 500, message: error.message }));
+          }
+
+          const user = results[0];
+
+          const token = jwt.sign(
+            { username: user.userName, role: user.roleID },
+            process.env.JWT_SECRET,
+            {
+              expiresIn: "1h",
+            }
+          );
+
+          res
+            .status(200)
+            .cookie("access_token", token, {
+              httpOnly: true,
+            })
+            .json({ username: user.userName, role: user.roleID });
+        }
+      );
     }
   );
 };
