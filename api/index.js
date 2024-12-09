b2f7208cd40306230ba97dcff9124fe336c5f750
jazwu <jaz.wu@outlook.com>
2024-12-08 22:49:52 -0500

    create a test API route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
-import db from "./db.js";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+
+app.use('/api/user', userRoutes);
