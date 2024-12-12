2c58be17dae09f1f3af1b4a58a94557751e55cc8
jazwu <jaz.wu@outlook.com>
2024-12-12 02:45:27 -0500

    feat(api): add create a new  item api

--- api/controllers/item.controller.js
+++ api/controllers/item.controller.js
@@ -44,3 +44,66 @@ export const getItems = (req, res, next) => {
     res.status(200).json({ items: results, total: results.length });
   });
 };
+
+export const createItem = (req, res, next) => {
+  const {
+    iDescription,
+    photo,
+    color,
+    isNew,
+    material,
+    mainCategory,
+    subCategory,
+  } = req.body.item;
+  const { donor, donateDate } = req.body.donation;
+  const pieces = req.body.pieces;
+
+  db.query(
+    "INSERT INTO Item (iDescription, photo, color, isNew, hasPieces, material, mainCategory, subCategory) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+    [
+      iDescription,
+      photo,
+      color,
+      isNew,
+      hasPieces,
+      material,
+      mainCategory,
+      subCategory,
+    ],
+    (error, results) => {
+      if (error) {
+        return next(error);
+      }
+      db.query(
+        "INSERT INTO DonatedBy (ItemID, userName, donateDate) VALUES (?, ?, ?)",
+        [results.insertId, donor, donateDate],
+        (error) => {
+          if (error) {
+            return next(error);
+          }
+        }
+      );
+      pieces.forEach((piece) => {
+        db.query(
+          "INSERT INTO Piece (ItemID, pieceNum, pDescription, length, width, height, roomNum, selfNum) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+          [
+            results.insertId,
+            piece.pieceNum,
+            piece.pDescription,
+            piece.length,
+            piece.width,
+            piece.height,
+            piece.roomNum,
+            piece.selfNum,
+          ],
+          (error) => {
+            if (error) {
+              return next(error);
+            }
+          }
+        );
+      });
+      res.status(201).json({ message: "Item created" });
+    }
+  );
+};
